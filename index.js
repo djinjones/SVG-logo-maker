ec6a25a656005efeb4a5de68f7f9481020068037
djinjones <160979104+djinjones@users.noreply.github.com>
2024-05-03 12:20:37 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+ const fs = require('fs');
+ const colors = require('colors');
+ const inquirer = require('inquirer');
+ const shape = reauire('./lib/shape.js');
+ const svg = require('./lib/svg.js');
