5eaf1326fbed97c8ca0403c997b78b92ca8e111b
djinjones <160979104+djinjones@users.noreply.github.com>
2024-05-03 15:23:00 -0600

    fixed issues with gradient

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const SVG = require('./lib/svg');
 const { Circle, Triangle, Square, Star } = require('./lib/shape');
 const shapeClasses = { Circle, Triangle, Square, Star };
 
-const svg = new SVG('circle', 'white', 'blue', 'ABC', 'blue', shapeClasses);
+const svg = new SVG('circle', 'white', 'white', 'ABC', 'blue', shapeClasses);
 svg.run();
 
