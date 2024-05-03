6e8b4614adf02f8eb531b3095fab709e5d96c21e
djinjones <160979104+djinjones@users.noreply.github.com>
2024-05-03 15:09:14 -0600

    added gradient functionality for shape fill

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const SVG = require('./lib/svg');
 const { Circle, Triangle, Square, Star } = require('./lib/shape');
 const shapeClasses = { Circle, Triangle, Square, Star };
 
-const svg = new SVG('circle', 'red', 'ABC', 'blue', shapeClasses);
+const svg = new SVG('circle', 'white', 'blue', 'ABC', 'blue', shapeClasses);
 svg.run();
+
