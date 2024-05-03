bf077d42ca9b3eb3b068a97b8e3db39233e7c4cc
djinjones <160979104+djinjones@users.noreply.github.com>
2024-05-03 14:04:51 -0600

    fixed length issue, svg now generates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
- const fs = require('fs');
- const colors = require('colors');
- const inquirer = require('inquirer');
- const shape = reauire('./lib/shape.js');
- const svg = require('./lib/svg.js');
 
+const SVG = require('./lib/svg');
+const { Circle, Triangle, Square, Star } = require('./lib/shape');
+const shapeClasses = { Circle, Triangle, Square, Star };
+
+const svg = new SVG('circle', 'red', 'ABC', 'blue', shapeClasses);
+svg.run();
